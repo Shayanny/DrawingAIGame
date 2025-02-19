2ac3c6d8a027e0346e728961dc744e592a3308ec
Shayanny <shayanny4@outlook.com>
2025-02-19 17:32:36 +0000

    Basic Canvas works and Fabric imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Canvas from "./components/Canvas";
 import Toolbar from "./components/Toolbar";
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,11 +1,36 @@
 
+import React, { useEffect, useRef } from 'react';
+import  {fabric}  from 'fabric';
+console.log(fabric); // Check the console for the fabric object
+
 const Canvas = () => {
-    return (
-      <div className="border border-gray-400 w-[500px] h-[500px] bg-white">
-        {/* Canvas will go here */}
-      </div>
-    );
-  };
-  
-  export default Canvas;
-  
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize the Fabric.js canvas
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      isDrawingMode: true, // Enable drawing mode
+      width: 800,
+      height: 600,
+      backgroundColor: '#f0f0f0',
+    });
+
+    // Add event listeners or other Fabric.js configurations here
+    canvas.on('mouse:up', () => {
+      console.log('Drawing stopped');
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      canvas.dispose();
+    };
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={canvasRef} id="canvas" />
+    </div>
+  );
+};
+
+export default Canvas;
